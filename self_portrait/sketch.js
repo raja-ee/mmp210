22eba9508c8cc40ea282ee0b8bc3ac94f8523b5d
Raja'ee <rhloney@gmail.com>
2020-02-04 17:36:00 -0500

    added selfportrait page, added p5

---
+++ self_portrait/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+	createCanvas(640,360);
+}
+
+function draw() {
+	background('black');
+	circle(mouseX, mouseY, 50);
+	circle(200, 160, 60)
+	circle(400, 160, 60)
+	rect(300, 220, 100, 30)
+}
