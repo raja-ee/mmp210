a92a9c14437d722571b2927bd3692ba99df89ee6
Raja'ee <rhloney@gmail.com>
2020-02-06 17:40:56 -0500

    configured sketch

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -4,8 +4,45 @@ function setup() {
 
 function draw() {
 	background('black');
-	circle(mouseX, mouseY, 50);
-	circle(200, 160, 60)
-	circle(400, 160, 60)
-	rect(300, 220, 100, 30)
+	
+	// The eyeballs
+	fill(0, 255, 0);
+	circle(200, 160, 60);
+	circle(400, 160, 60);
+	fill(0);
+	circle(200, 160, 30);
+	circle(400, 160, 30);
+	
+	// The mouth
+	fill(0)
+	rect(250, 280, 100, 30, 20);
+	triangle(300, 130, 280, 230, 320, 230);
+	
+	// Part of the frame on my actual glasses
+	fill(255);
+	circle(265, 160, 10);
+	circle(335, 160, 10);
+	circle(135, 160, 10);
+	circle(465, 160, 10);
+	
+	// My glasses
+	noFill();
+	stroke(255, 255, 255);
+	circle(200, 160, 150);
+	circle(400, 160, 150);
+	line(265, 160, 335, 160);
+
+	// My eyebrows
+	triangle(265, 80, 135, 100, 275, 100);
+
+	// The mouse pointer, with a color change based on the click of the mouse
+	if (mouseIsPressed) {
+		fill(0, 255, 0);
+	square(mouseX, mouseY, 20, 0, 20, 20, 20);
+	} else {
+		fill(255);
+	square(mouseX, mouseY, 20, 0, 20, 20, 20);
+	}
+	
+
 }
