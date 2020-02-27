16f49a17f694782a0a7d728f20d0c3f2b8c756ee
Raja'ee <rhloney@gmail.com>
2020-02-27 17:37:52 -0500

    added memes on top of memes on top of memes

--- meme_1/sketch.js
+++ meme_1/sketch.js
@@ -2,6 +2,16 @@
     Meme V1 - 02.25.2020
 */
 
+// global scope
+var Meme;
+var MouseIMG;
+var CNTR = 250
+
+function preload() {
+	Meme = loadImage("memeBKGD.jpg");
+	MouseIMG = loadImage("angrysmiley.png");
+}
+
 function setup() {
 	createCanvas(500,500); // Canvas was centered in styles.css lines 40-49 from this website: http://fh-potsdam.github.io/steel-ant-input-output/examples/center-canvas/
 
@@ -10,10 +20,13 @@ function setup() {
 function draw() {
 	background('white');
 
+	// Background for the meme on the bottom half of the meme
+	image(Meme, 0, 200, 500, 300);
+
 	fill(0);
 	noStroke();
-	rect(0, 200, 500, 300);
 
+	// Lines 30-36 are a very common meme format online
 	textAlign(LEFT, CENTER);
 	textSize(35);
 	textStyle(BOLD);
@@ -22,18 +35,25 @@ function draw() {
 
 	text('Memes:', 30, 160);
 
-	fill(255);
+	// A joke for avid meme searchers
+	fill(0);
 	textAlign(CENTER);
 	textStyle(NORMAL);
 	textSize(15);
-	text('*relatable image*', 250, 340)
+	text('*insert relatable image*', CNTR, 345)
 
+	// Lines 46-55 is the "Captain Obvious" text for this meme. Line 47 has a color extracted directly from the meme background used in the stroke.
+	strokeWeight(5);
+	stroke(251, 216, 142);
 	textAlign(CENTER);
 	textSize(38);
 	textStyle(BOLD);
 	textFont('Impact');
-	text('AH YES, INTELLECTUAL COMEDY', 250, 250);
+	text('AH YES, INTELLECTUAL COMEDY', CNTR, 250);
 
 	textSize(50);
-	text('SO FUNNY, MUST LAUGH', 250, 450)
+	text('SO FUNNY, MUST LAUGH', CNTR, 450)
+
+	// Some icing on top of the cake
+	image(MouseIMG, mouseX, mouseY, 125, 86.25)
 }
